db3237a12a881142023cbbdcc91b5737bbc47b29
FirzaqEmirPranowo <firzaqemirpranowo@gmail.com>
2022-04-26 09:01:35 +0700

    miniw

--- resources/views/pages/partial/footer.blade.php
+++ resources/views/pages/partial/footer.blade.php
@@ -1,14 +1,14 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      <p class="text-center" style="margin-top: -60px;" id="copyright">Copyright &copy; 2022. PEMERINTAH KABUPATEN MADIUN <a href="" target="_blank"></a></p>
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> --}}
     </div>
   </footer><!-- End Footer -->
 
