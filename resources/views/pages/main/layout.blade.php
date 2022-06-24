55cf242033832ba301e7c063e623e307a4946498
evaleries <didaarda15@gmail.com>
2022-06-24 14:27:03 +0700

    fix swal undefined

--- resources/views/pages/main/layout.blade.php
+++ resources/views/pages/main/layout.blade.php
@@ -22,6 +22,7 @@
     @include('pages.partial.footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
     <script>$.ajaxSetup({headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'}});const Toast = Swal.mixin({toast: true, position: 'top-end', showConfirmButton: false, timer: 3000, timerProgressBar: true, didOpen: (toast) => {toast.addEventListener('mouseenter', Swal.stopTimer);toast.addEventListener('mouseleave', Swal.resumeTimer)}});</script>
 
     <!-- Vendor JS Files -->
