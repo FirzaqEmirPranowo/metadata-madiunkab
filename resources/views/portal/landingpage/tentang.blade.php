c1650a8eba19d47acfe023fe9d6a896ffff9b150
evaleries <didaarda15@gmail.com>
2022-07-18 15:10:17 +0700

    fix landing page

--- resources/views/portal/landingpage/tentang.blade.php
+++ resources/views/portal/landingpage/tentang.blade.php
@@ -1,23 +1,22 @@
-@include ('../portal/partial/header')
+@extends('portal.landingpage.layout')
 
-        <div class="container">
-            <div class="page-banner home-banner">
-                <div class="row align-items-center flex-wrap-reverse h-100">
-                    <div class="col-md-6 py-5 wow fadeInLeft">
-                        <h1 class="mb-4">Halaman Tentang</h1>
-                        <!-- <p class="text-lg text-grey mb-5">Di sini Anda bisa akses koleksi data dan artikel terlengkap di Kabupaten Madiun dengan cepat, mudah, dan akurat dibantu berbagai fitur bermanfaat.</p> -->
-                        <!-- <a href="#cari" data-role="smoothscroll" class="btn btn-primary btn-split">Cari Data <div class="fab"><span class="mai-play"></span></div></a> -->
-                    </div>
-                    <div class="col-md-6 py-5 wow zoomIn">
-                        <div class="img-fluid text-center">
-                            <img src="../landing-assets/img/banner_image_1.svg" alt="">
-                        </div>
+@section('header')
+
+    <div class="container">
+        <div class="page-banner home-banner">
+            <div class="row align-items-center flex-wrap-reverse h-100">
+                <div class="col-md-6 py-5 wow fadeInLeft">
+                    <h1 class="mb-4">Tentang Satu Data</h1>
+                    <p>SatuData Kabupaten Madiun merupakan portal data yang dapat diakses oleh publik untuk keperluan umum dan riset teknologi.</p>
+                </div>
+                <div class="col-md-6 py-5 wow zoomIn">
+                    <div class="img-fluid text-center">
+                        <img src="../landing-assets/img/banner_image_1.svg" alt="">
                     </div>
                 </div>
-                <a href="#cari" class="btn-scroll" data-role="smoothscroll"><span class="mai-arrow-down"></span></a>
             </div>
+            <a href="#cari" class="btn-scroll" data-role="smoothscroll"><span class="mai-arrow-down"></span></a>
         </div>
-    </header>
+    </div>
 
-
-    @include ('../portal/partial/footer')
+@endsection
